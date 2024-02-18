bb0fab9e2f8d69a3036bf1d19fca6fb9c380f828
BobkovValeriy <darkmito17051990@gmail.com>
2024-02-18 18:38:25 +0200

    workedLogin

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,30 +2,32 @@ import { useState } from "react";
 import "./Login.css";
 import axios from "axios";
 
-async function checkLogin(username, password, setLogined) {
-  try {
-    const apiEndpoint =
-      "https://eu-central-1.aws.data.mongodb-api.com/app/data-yjqvx/endpoint/checklogin";
+  async function checkLogin(username, password, setLogined) {
+    try {
+      console.log(username, password);
+      const apiEndpoint = `https://eu-central-1.aws.data.mongodb-api.com/app/data-yjqvx/endpoint/checklogin?username=${encodeURIComponent(
+        username
+      )}&password=${encodeURIComponent(password)}`;
 
-    // Выполняем запрос к API
-    const response = await axios.post(apiEndpoint, {
-      username: username,
-      password: password,
-    });
+      const response = await axios.get(apiEndpoint);
 
-    // Проверяем, успешен ли запрос
-    if (response.status === 204) {
-      // Статус 204 - успешный запрос без возвращаемых данных
-      return setLogined(true); // Возвращаем результат успешной проверки
-    } else {
-      console.error("Некорректный ответ от сервера");
-      return { error: "Некорректный ответ от сервера" };
+      console.log(response); // Выводим ответ сервера в консоль
+
+      // Проверяем, успешен ли запрос и код в ответе равен "logined"
+      if (response.data.login === "logined") {
+        return setLogined(true);
+      } else if (response.data.login === "notlogined") {
+        console.error("Неверные учетные данные");
+        return { error: "Неверные учетные данные" };
+      } else {
+        console.error("Некорректный ответ от сервера", response.data);
+        return { error: "Некорректный ответ от сервера" };
+      }
+    } catch (error) {
+      console.error("Ошибка при выполнении запроса:", error.message);
+      return { error: error.message };
     }
-  } catch (error) {
-    console.error("Ошибка при выполнении запроса:", error.message);
-    return { error: error.message };
   }
-}
 
 function Login({ logined, setLogined }) {
   const [userName, setUserName] = useState("");
